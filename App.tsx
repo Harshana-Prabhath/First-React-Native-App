63fb738d7583652b01846e183813d329261a753f
BinaryWizardX <ict20930@fot.sjp.ac.lk>
2024-11-13 23:31:21 +0530

    use state learned

--- App.tsx
+++ App.tsx
@@ -1,45 +1,78 @@
-
-import React from 'react';
+import React, { useState } from 'react';
 import TextComponent from './Components/TextComponent/TextComponent';
-
 import {
   SafeAreaView,
-
-Text,
-StyleSheet,
-Dimensions,
+  Text,
+  StyleSheet,
+  Dimensions,
+  View,
+  Button,
+  TouchableOpacity,
 } from 'react-native';
 
-
-
-
-
-const {height} = Dimensions.get('window');
-
+const { height, width } = Dimensions.get('window');
 
 function App(): React.JSX.Element {
-
-
-
+  const [value, setValue] = useState(0);
 
   return (
     <SafeAreaView style={style.containerStyle}>
-    <Text style={style.textColor}>THis is the first text tag</Text>
-    <TextComponent TextName="My name is Harshana" />
+      <View style={style.contentContainer}>
+        <Text style={style.valueStyle}>{value}</Text>
+        <View style={style.buttonView}>
+          <TouchableOpacity style={style.buttonSub} onPress={() => setValue(value - 1)}>
+            <Text style={style.textSubAdd}>Substract</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={style.buttonAdd} onPress={() => setValue(value + 1)}>
+            <Text style={style.textSubAdd}>Add</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
 
 const style = StyleSheet.create({
   containerStyle: {
-    backgroundColor: '#808b96',
     height: height,
+    width: width,
   },
-  textColor: {
-    color: '#e74c3c',
+  contentContainer: {
+    height: height / 3,
+    backgroundColor: '#D3D3D3',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  valueStyle: {
+    fontSize: 50,
+  },
+  buttonView: {
+    marginTop: 50,
+    paddingHorizontal: 40,
+    width: width,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    gap: 50,
+  },
+  buttonAdd: {
+    backgroundColor: 'green',
+    width: 80,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonSub: {
+    width: 80,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'red',
+    fontSize: 80,
+  },
+  textSubAdd: {
+    fontSize: 15,
+    color: 'white',
   },
 });
-
-
 
 export default App;
--- Components/TextComponent/TextComponent.tsx
+++ Components/TextComponent/TextComponent.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Alert, Text } from 'react-native';
 import TextComponentStyleSheet from './Stylesheets/TextComponentStyles';
 
-
 type Props = {
-TextName?: string,
-}
+  TextName?: string;
+};
 
-
-const TextComponent = ({TextName}: Props)=>{
-return (
-<Text style={TextComponentStyleSheet.nameText}>{TextName ? `${TextName}` : 'This is a custom text'}</Text>
-);
+const TextComponent = ({ TextName }: Props) => {
+  return (
+    <Text
+      onPress={() => {
+        Alert.alert('The text is clicked');
+      }}
+      style={TextComponentStyleSheet.nameText}>
+      {TextName ? `${TextName}` : 'This is a custom text'}
+    </Text>
+  );
 };
 
 export default TextComponent;
